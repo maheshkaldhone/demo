6c056f700b66604bdefe41d7693e0f486045b100
MAHESH KALDHONE <101930531+maheshkaldhone@users.noreply.github.com>
2023-09-03 11:45:27 +0530

    Update app.mjs

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -16,7 +16,7 @@ export const lambdaHandler = async (event, context) => {
         return {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world from lambda pipeline',
+                message: 'hello world again from lambda pipeline',
             })
         }
     } catch (err) {
